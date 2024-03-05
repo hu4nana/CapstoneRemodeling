378a9037693ddef08eacfebc1e6aec3b0569ecd2
hu4nana <112612929+hu4nana@users.noreply.github.com>
2024-03-06 07:18:35 +0900

    BasicAction, AniLayer 세분화

--- CapstoneRemodeling/Assets/Remodeling/Scripts/Player/PlayerBasicAction.cs
+++ CapstoneRemodeling/Assets/Remodeling/Scripts/Player/PlayerBasicAction.cs
@@ -1,29 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public interface IPlayerBasicAction
 {
-    public void Move(float moveSpeed, Rigidbody rigid,GameObject player)
+    public void Move(float moveSpeed, Rigidbody rigid,GameObject player,Animator ani)
     {
-        float xInput = Input.GetAxis("Horizontal");
+        
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            float xInput = Input.GetAxis("Horizontal");
+            float xDir = xInput;
 
-        int xDir = (int)xInput;
-        player.transform.rotation = Quaternion.Slerp(player.transform.rotation, Quaternion.LookRotation(
-                Vector3.right * xDir), Time.deltaTime * 24);
-        rigid.velocity = new Vector3(xDir * moveSpeed, rigid.velocity.y, 0);
-        //if (g_isFloor)
-        //{
-        //    g_ani.SetBool("isWalk", true);
-        //}
-        rigid.velocity = new Vector3(xInput * moveSpeed, 0, 0);
-        Debug.Log("¿òÁ÷ÀÓ");
+            rigid.velocity = new Vector3(xDir * moveSpeed, rigid.velocity.y, 0);
+
+            ani.SetBool("isRun", true);
+
+            if(xDir!=0)
+            {
+                player.transform.rotation = Quaternion.Slerp(player.transform.rotation, 
+                    Quaternion.LookRotation(Vector3.right * xDir), Time.deltaTime * 24);
+            }
+        }
+        else
+        {
+            ani.SetBool("isRun", false);
+        }
     }
-    public bool Crouch()
+    public void Attack(float Dmage, Rigidbody rigid,GameObject player, Animator ani)
     {
-        bool yInput = Input.GetKey(KeyCode.DownArrow);
-        bool isCrouch = yInput;
-        return isCrouch;
+        if(Input.GetKey(KeyCode.X))
+        {
+            ani.SetBool("isAttack",true);
+        }
+        else
+        {
+            ani.SetBool("isAttack", false);
+        }
     }
-    //public void BasicAttack();
+    public void Crouch(Animator ani)
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            ani.SetBool("isCrouch", true);
+        }
+        else
+            ani.SetBool("isCrouch", false);
+    }
 }
--- CapstoneRemodeling/Assets/Remodeling/Scripts/Player/PlayerController.cs
+++ CapstoneRemodeling/Assets/Remodeling/Scripts/Player/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour,IPlayerBasicAction
+public class PlayerController : MonoBehaviour, IPlayerBasicAction
 {
 
     [SerializeField]
@@ -11,41 +11,31 @@ public class PlayerController : MonoBehaviour,IPlayerBasicAction
     Rigidbody rigid;
     Animator ani;
 
-    IPlayerBasicAction basicAction;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
-        ani=GetComponent<Animator>();
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //float xInput = Input.GetAxis("Horizontal");
-
-        //int xDir = (int)xInput;
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(
-        //        Vector3.right * xDir), Time.deltaTime * 24);
-        //rigid.velocity = new Vector3(xDir * 5, rigid.velocity.y, 0);
-        ////if (g_isFloor)
-        ////{
-        ////    g_ani.SetBool("isWalk", true);
-        ////}
-        //rigid.velocity = new Vector3(xInput * 5, 0, 0);
-        //Debug.Log("¿òÁ÷ÀÓ");
         Move();
+        Attack();
+        Crouch();
     }
     void Move()
     {
-        //GetComponent<PlayerBasicAction>().Move(speed, rigid);
-        basicAction.Move(4, rigid,this.gameObject);
+        GetComponent<IPlayerBasicAction>().Move(speed, rigid, gameObject, ani);
+    }
+    void Attack()
+    {
+        GetComponent<IPlayerBasicAction>().Attack(3, rigid, gameObject, ani);
     }
     void Crouch()
     {
-        basicAction.GetHashCode();
+        GetComponent<IPlayerBasicAction>().Crouch(ani);
     }
 }

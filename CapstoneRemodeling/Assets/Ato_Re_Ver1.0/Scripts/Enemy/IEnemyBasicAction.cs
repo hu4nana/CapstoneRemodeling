0177e296af912a6a5041837e3876eeb391aec9d0
hu4nana <112612929+hu4nana@users.noreply.github.com>
2024-03-12 10:45:58 +0900

    Add Assets / Add EnemyBasicAction

--- CapstoneRemodeling/Assets/Ato_Re_Ver1.0/Scripts/Enemy/IEnemyBasicAction.cs
+++ CapstoneRemodeling/Assets/Ato_Re_Ver1.0/Scripts/Enemy/IEnemyBasicAction.cs
@@ -49,6 +49,7 @@ public interface IEnemyBasicAction
 
         bool isActive = true;
 
+
         if(timer < 0)
         {
             timer = triggerTimer;
@@ -67,11 +68,61 @@ public interface IEnemyBasicAction
         }
         ani.SetBool("isTrigger", isActive);
     }
-
-    // 투사체 생성
+   
+    
+    // 전방에 투사체 발사
     public void Shoot()
     {
 
+    }
+
+    // 범위 내의 Target한테 돌진
+    public void ChargeToTarget(float startSpeed,GameObject own, Rigidbody rigid,LayerMask collisionLayer)
+    {
+        RaycastHit hit;
+        float accelation = 1.2f;
+        int maxSpeed = 10;
+        if (Physics.Raycast(own.transform.position,
+            Vector3.right * (own.transform.rotation.y / Mathf.Abs(own.transform.rotation.y))
+            , out hit, 5))
+        {
+            if (hit.collider.gameObject.layer == 10)
+            {
+                
+                if (rigid.velocity.x >= maxSpeed)
+                {
+                    rigid.velocity = new Vector3(maxSpeed, rigid.velocity.y, 0);
+                }
+                else
+                {
+                    rigid.velocity += new Vector3(startSpeed * accelation, rigid.velocity.y, 0);
+                }
+                    
+            }
+            if (hit.collider.gameObject.layer != 10)
+            {
+                rigid.velocity=new Vector3(0,rigid.velocity.y, 0);
+            }
+        }
+    }    
+
+    // 범위 내의 Target한테 거리두기
+    public void KeepDistanceToTarget(float distance,GameObject own,Rigidbody rigid)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(own.transform.position,
+            Vector3.right * (own.transform.rotation.y / Mathf.Abs(own.transform.rotation.y))
+            , out hit, 5))
+        {
+            if (hit.collider.gameObject.layer == 10)
+            {
+
+            }
+            if (hit.collider.gameObject.layer != 10)
+            {
+                rigid.velocity = new Vector3(0, rigid.velocity.y, 0);
+            }
+        }
     }
 
     // 플레이어에게 계속 돌진 feat 관성
@@ -82,5 +133,30 @@ public interface IEnemyBasicAction
     public void InertiaMove()
     {
 
+    }
+
+
+    // Target을 매개변수로 받아오기
+    public void Charge(GameObject target,GameObject own, Rigidbody rigid,float endTimer)
+    {
+        int minSpeed = 1;
+        int maxSpeed = 15;
+        float accelation = 1.1f;
+        if(target!=null)
+        {
+            rigid.velocity += new Vector3(minSpeed * accelation, rigid.velocity.y, 0);
+        }
+        else
+        {
+            endTimer-=Time.deltaTime;
+            if (endTimer <= 0)
+            {
+                rigid.velocity=new Vector3(0,rigid.velocity.y, 0);
+            }
+        }
+        if (rigid.velocity.x >= maxSpeed)
+        {
+            rigid.velocity = new Vector3(maxSpeed, rigid.velocity.y, 0);
+        }
     }
 }
--- CapstoneRemodeling/Assets/Ato_Re_Ver1.0/Scripts/Enemy/TestEnemy.cs
+++ CapstoneRemodeling/Assets/Ato_Re_Ver1.0/Scripts/Enemy/TestEnemy.cs
@@ -7,7 +7,7 @@ public class TestEnemy : MonoBehaviour, IEnemyBasicAction
 {
 
     Rigidbody rigid;
-
+    GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +17,34 @@ public class TestEnemy : MonoBehaviour, IEnemyBasicAction
     // Update is called once per frame
     void Update()
     {
-        OneDirectionMove();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,
+            Vector3.right * (transform.rotation.y / Mathf.Abs(transform.rotation.y))
+            , out hit, 5))
+        {
+            if (hit.collider.gameObject.layer == 10)
+            {
+                target = hit.collider.gameObject;
+
+            }
+            if (hit.collider.gameObject.layer != 10)
+            {
+                target = null;
+            }
+        }
+
+        GetComponent<IEnemyBasicAction>().Charge(target, gameObject, rigid, 3);
+        //OneDirectionMove();
+        //ChargeToTarget();
     }
+
+    // One Direction Move_LIke Goomba
     void OneDirectionMove()
     {
         GetComponent<IEnemyBasicAction>().OneDirectionMove(3,rigid,gameObject,0);
+    }
+    void ChargeToTarget()
+    {
+        GetComponent<IEnemyBasicAction>().ChargeToTarget(1,gameObject, rigid, 6);
     }
 }
